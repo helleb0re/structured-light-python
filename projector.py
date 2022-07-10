ea1248d41ec00ae3b54df208f3237e2a0b3fba21
Антон Поройков <poroykovay@gmail.com>
2022-07-10 15:01:46 +0300

    Projector class fixed and updated

--- projector.py
+++ projector.py
@@ -1,4 +1,5 @@
 '''Module for Projector class'''
+from __future__ import annotations
 
 import cv2
 import numpy as np
@@ -19,33 +20,28 @@ class Projector():
         '''
         Open new window thru OpenCV GUI and show it on second extended screen
         '''
-        cv2.namedWindow('projection', cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty('projection', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow('Projector window', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('Projector window', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         # TODO: remove magic number 1920 with screen resolution for multimonitor case
         # https://stackoverflow.com/questions/3129322/how-do-i-get-monitor-resolution-in-python
         cv2.moveWindow('Projector window', 1920, 0)
         self.window_exist = True
 
-    def project_pattern(self, pattern: np.array) -> np.array:
+    def project_pattern(self, pattern: np.ndarray) -> None:
         '''
         Project pattern thru OpenCV GUI window, before projection pattern is intensity corrected
 
         Args:
             pattern (numpy array): image to project with OpenCV imshow method
-        
-        Returns:
-            corrected_pattern (numpy array): corrected image for test purpose
         '''
         # Open OpenCV GUI window, if it is has not been already opened
         if not self.window_exist:
             self.set_up_window()
         
         # Correct image with calibration coefficients
-        corrected_pattern = self.image_brightness_rescale_factor * (pattern + 1) + self.min_image_brightness
+        self._corrected_pattern = self.image_brightness_rescale_factor * (pattern + 1) + self.min_image_brightness
         # Show image at OpenCV GUI window
-        cv2.imshow('projection', corrected_pattern.astype(np.uint8))
-
-        return corrected_pattern
+        cv2.imshow('Projector window', self._corrected_pattern.astype(np.uint8))
 
     def close_window(self) -> None:
         '''
@@ -53,6 +49,13 @@ class Projector():
         '''
         cv2.destroyWindow('Projector window')
         self.window_exist = False
+
+    @property
+    def corrected_pattern(self) -> np.ndarray:
+        '''
+        Return last projected corrected pattern as numpy array
+        '''
+        return self._corrected_pattern
 
     @property
     def resolution(self) -> tuple[int, int]:
