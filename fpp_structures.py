43136d46d4327a759bef76dd1b239ecf31483d64
Антон Поройков <poroykovay@gmail.com>
2022-07-25 09:27:49 +0300

    Fringe orientation added to FPPMeasurement

--- fpp_structures.py
+++ fpp_structures.py
@@ -9,15 +9,16 @@ from typing import Optional
 @dataclass
 class FPPMeasurement:
     '''Class to store FPP measurement data'''
-    frequencies : list[float]
-    shifts : list[float]
-    imgs_file_names : list[list[str]]
+    frequencies: list[float]
+    shifts: list[float]
+    imgs_file_names: list[list[str]]
+    fringe_orientation: Optional[str] = 'vertical'
     imgs_list: Optional[list[list[np.array]]] = None
 
     @property
-    def frequency_counts(self) -> int :
+    def frequency_counts(self) -> int:
         return len(self.frequencies)
 
     @property
-    def shifts_count(self) -> int :
+    def shifts_count(self) -> int:
         return len(self.shifts)
